c51c28291c86ce0469989554bb9de67c05716628
Gerry William Nanlohy <76998095+GWENTzy@users.noreply.github.com>
2022-05-29 18:16:05 +0700

    The Basics

---
+++ TypeAnnotations.swift
@@ -0,0 +1,13 @@
+import Swift
+
+//int = integer
+let welcome = 42
+
+//double and float = point values
+let pi = 3.14159
+
+//bool = boolean values
+
+//string = textual data
+var welcomeMSG: string
+welcomeMSG = "Hello"
