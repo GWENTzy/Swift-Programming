f029e47c62c5f03fd3d5fe1f4a685b39852b1f3e
Gerry William Nanlohy <76998095+GWENTzy@users.noreply.github.com>
2022-05-29 18:13:38 +0700

    The Basics

---
+++ Introduction.swift
@@ -0,0 +1,12 @@
+import Swift
+
+print("Hello, World!")
+
+print("The Value is \(myVariable)")
+
+print(myVariable)
+
+// this is a comment
+
+/* this is also comment,
+but written over multiple lines*/
