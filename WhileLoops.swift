c0b7baf7548388eb989720873b98ea97e950c6a2
Gerry William Nanlohy <76998095+GWENTzy@users.noreply.github.com>
2022-05-29 18:16:25 +0700

    Control Flow & Collections

---
+++ WhileLoops.swift
@@ -0,0 +1,24 @@
+import Swift
+
+//while loop
+while a < b {
+    print(a)
+    a+=1
+}
+
+var x = 10
+while x > 0 {
+    print(x)
+    x-=1
+}
+
+//repeat while
+repeat {
+    x-=1
+} while x > 0
+
+var i = 3, n = 3
+repeat {
+    print(i)
+    i+=1
+} while i < n
