01458cf5083eaf1dca88a2a35fb18f85d3d1680b
Gerry William Nanlohy <76998095+GWENTzy@users.noreply.github.com>
2022-05-29 18:14:30 +0700

    The Basics

---
+++ Optional.swift
@@ -0,0 +1,13 @@
+import Swift
+
+var myCode: Int? = 404
+
+var test: String?
+
+//nil
+var myCode: Int? = 404
+myCode = nil
+//myCode now contains no value
+
+var someMSG: String?
+//someMSG is automatically set to nil
